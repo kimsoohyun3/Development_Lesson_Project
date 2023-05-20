6c3eb4f2710376611261b777f388df2405e0e5c9
LeeJaewon <jaewon971111@gmail.com>
2023-05-20 11:10:48 +0900

    feat : 회원가입 컨트롤러 개발

---
+++ lesson/src/main/java/project/lesson/controller/MemberController.java
@@ -0,0 +1,44 @@
+package project.lesson.controller;
+
+import javax.validation.Valid;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
+import io.swagger.annotations.ApiResponse;
+import io.swagger.annotations.ApiResponses;
+import project.lesson.dto.member.MemberSaveRequestDto;
+import project.lesson.dto.member.MemberSaveResponseDto;
+import project.lesson.service.MemberService;
+
+@Api(tags = "회원 관련 API")
+@RestController
+public class MemberController {
+	private final MemberService memberService;
+
+	@Autowired
+	public MemberController(MemberService memberService) {
+		this.memberService = memberService;
+	}
+
+	@ApiOperation(
+			value = "회원가입",
+			notes = "회원가입을 진행합니다."
+	)
+	@ApiResponses(
+			{
+					@ApiResponse(code = 200, message = "회원가입 성공", response = MemberSaveResponseDto.class)
+			}
+	)
+	@PostMapping("/member/join")
+	public ResponseEntity<MemberSaveResponseDto> saveMember(
+			@RequestBody @Valid MemberSaveRequestDto memberSaveRequestDto
+	) {
+		return ResponseEntity.ok().body(memberService.saveMember(memberSaveRequestDto));
+	}
+}
