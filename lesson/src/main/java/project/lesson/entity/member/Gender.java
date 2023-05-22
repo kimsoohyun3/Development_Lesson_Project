d2d13d2c2fb9a4d3456c3026c67071c5c79e6f49
LeeJaewon <jaewon971111@gmail.com>
2023-05-22 13:02:24 +0900

    feat : 회원 가입 엔티티 설계

---
+++ lesson/src/main/java/project/lesson/entity/member/Gender.java
@@ -0,0 +1,4 @@
+package project.lesson.entity.member;
+
+public enum Gender {
+}
---
+++ lesson/src/main/java/project/lesson/entity/member/Member.java
@@ -0,0 +1,60 @@
+package project.lesson.entity.member;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.EnumType;
+import javax.persistence.Enumerated;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Getter
+@Table(name = "MEMBER")
+@NoArgsConstructor
+public class Member {
+	@Id
+	@Column(name = "ID")
+	String id;
+
+	@Column(name = "PASSWORD", nullable = false)
+	String password;
+
+	@Column(name = "EMAIL", nullable = false)
+	String email;
+
+	@Column(name = "NICKNAME", nullable = false)
+	String nickname;
+
+	@Column(name = "GENDER", nullable = false)
+	@Enumerated(EnumType.STRING)
+	Gender gender;
+
+	@Column(name = "USER_CLASSIFICATION", nullable = false)
+	@Enumerated(EnumType.STRING)
+	UserClassification userClassification;
+
+	@Column(name = "AGE_GROUP", nullable = false)
+	@Enumerated(EnumType.STRING)
+	AgeGroup ageGroup;
+
+	@Builder
+	public Member(
+			String id, String password, String email,
+			String nickname, Gender gender,
+			UserClassification userClassification,
+			AgeGroup ageGroup
+	) {
+		this.id = id;
+		this.password = password;
+		this.email = email;
+		this.nickname = nickname;
+		this.gender = gender;
+		this.userClassification = userClassification;
+		this.ageGroup = ageGroup;
+	}
+}
+
