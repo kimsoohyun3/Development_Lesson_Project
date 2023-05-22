57c2d961d42c7ee9d46747f3e76bd3d5b07630cb
LeeJaewon <jaewon971111@gmail.com>
2023-05-22 13:01:50 +0900

    feat : cors 설정

---
+++ lesson/src/main/java/project/lesson/config/WebConfig.java
@@ -0,0 +1,20 @@
+package project.lesson.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class WebConfig implements WebMvcConfigurer {
+
+	@Override
+	public void addCorsMappings(CorsRegistry corsRegistry) {
+		corsRegistry.addMapping("/**")
+				.allowedOrigins("*")
+				.allowedMethods("*")
+				.allowedHeaders("*")
+				.allowCredentials(true)
+				.maxAge(3600);
+	}
+}
