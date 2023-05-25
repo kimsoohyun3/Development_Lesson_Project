457548230943182e5402c7dcdb14f14a126aa0b2
LeeJaewon <jaewon971111@gmail.com>
2023-05-25 16:07:28 +0900

    refactor : MemberController 스웨거 설정

--- lesson/src/main/java/project/lesson/controller/MemberController.java
+++ lesson/src/main/java/project/lesson/controller/MemberController.java
@@ -9,6 +9,9 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
+import io.swagger.annotations.ApiResponse;
+import io.swagger.annotations.ApiResponses;
 import project.lesson.dto.member.MemberSaveRequestDto;
 import project.lesson.dto.member.MemberSaveResponseDto;
 import project.lesson.service.MemberService;
@@ -23,6 +26,15 @@ public class MemberController {
 		this.memberService = memberService;
 	}
 
+	@ApiOperation(
+			value = "회원가입",
+			notes = "회원가입을 진행합니다."
+	)
+	@ApiResponses(
+			{
+					@ApiResponse(code = 200, message = "회원가입", response = MemberSaveResponseDto.class)
+			}
+	)
 	@PostMapping("/member/join")
 	public ResponseEntity<MemberSaveResponseDto> joinMember(
 			@RequestBody @Valid MemberSaveRequestDto memberSaveRequestDto
