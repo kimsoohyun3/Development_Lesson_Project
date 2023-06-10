b4f8db96361f3a424a98e171c3816e1ae81d85a4
LeeJaewon <jaewon971111@gmail.com>
2023-06-10 19:36:02 +0900

    refactor : swagger config

--- lesson/src/main/java/project/lesson/config/security/WebSecurityConfig.java
+++ lesson/src/main/java/project/lesson/config/security/WebSecurityConfig.java
@@ -38,7 +38,6 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
 						"/api/v1/auth/**", "/",
 						"/v2/api-docs", "/swagger-resources/**", "/swagger-ui/index.html", "/swagger-ui.html",
 						"/webjars/**", "/swagger/**",
-						"/h2-console/**",
 						"/favicon.ico"
 				).permitAll()
 				.anyRequest()
@@ -56,6 +55,10 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
 	@Override
 	public void configure(WebSecurity web) throws Exception {
 		web.debug(true);
+		web.ignoring().antMatchers("/v2/api-docs", "/configuration/ui",
+				"/swagger-resources", "/configuration/security",
+				"/swagger-ui/index.html", "/webjars/**", "/swagger/**",
+				"/swagger-resources/**");
 	}
 
 	@Bean
