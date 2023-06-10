40d6ba044d7bb440067e8cb17036abfdde2accb5
LeeJaewon <jaewon971111@gmail.com>
2023-06-10 19:56:32 +0900

    refactor : security config

--- lesson/src/main/java/project/lesson/config/security/CustomAuthenticationEntryPoint.java
+++ lesson/src/main/java/project/lesson/config/security/CustomAuthenticationEntryPoint.java
@@ -8,11 +8,13 @@ import javax.servlet.http.HttpServletResponse;
 
 import org.springframework.security.core.AuthenticationException;
 import org.springframework.security.web.AuthenticationEntryPoint;
+import org.springframework.stereotype.Component;
 
+@Component
 public class CustomAuthenticationEntryPoint implements AuthenticationEntryPoint {
 	@Override
 	public void commence(HttpServletRequest request, HttpServletResponse response,
-			AuthenticationException authException) throws IOException, ServletException {
+			AuthenticationException authException) throws IOException {
 		//ObjectMapper objectMapper = new ObjectMapper();
 		/*ExceptionMessage message = new ExceptionMessage(
 				authException.getClass().getSimpleName(),
