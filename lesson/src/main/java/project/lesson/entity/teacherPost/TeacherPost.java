c1d451400a6621f78c66cad7fb39ebd61e2d7792
LeeJaewon <jaewon971111@gmail.com>
2023-06-16 21:21:24 +0900

    refactor : git error

---
+++ lesson/src/main/java/project/lesson/entity/teacherPost/TeacherPost.java
@@ -0,0 +1,63 @@
+package project.lesson.entity.teacherPost;
+
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import project.lesson.entity.base.BaseEntity;
+import project.lesson.entity.commonEnum.OnOrOff;
+import project.lesson.entity.commonEnum.Subject;
+import project.lesson.entity.member.Member;
+
+import javax.persistence.*;
+
+@Entity
+@Getter
+@NoArgsConstructor
+public class TeacherPost extends BaseEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "teacherPost_id")
+    private Long id;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    private Member writer; // 작성자
+
+    @Column(nullable = false)
+    private String title; // 제목
+
+    @Column(nullable = false)
+    private String content; // 내용
+
+    @Column(nullable = false)
+    @Enumerated(EnumType.STRING)
+    private Subject subject; // 과목
+
+    @Column(nullable = false)
+    private String area; // 지역
+
+    @Column(nullable = false)
+    @Enumerated(EnumType.STRING)
+    private OnOrOff onOrOff; // 과외 온 / 오프 여부
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "ID")
+    private Member member; // 회원
+
+    @Builder
+    public TeacherPost(String title, String content, Subject subject, String area, OnOrOff onOrOff) {
+        this.title = title;
+        this.content = content;
+        this.subject = subject;
+        this.area = area;
+        this.onOrOff = onOrOff;
+    }
+
+    public void updatePost(String title, String content, Subject subject, String area, OnOrOff onOrOff) {
+        this.title = title;
+        this.content = content;
+        this.subject = subject;
+        this.area = area;
+        this.onOrOff = onOrOff;
+    }
+}
