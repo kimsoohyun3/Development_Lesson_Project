135495ac5cd0f5868dee74caebb858f3357d9c82
이재원 <jaewon971111@gmail.com>
2023-07-02 19:49:25 +0900

    update jar

--- lesson/src/main/java/project/lesson/controller/MemberController.java
+++ lesson/src/main/java/project/lesson/controller/MemberController.java
@@ -38,9 +38,9 @@ public class MemberController {
 
 	@Autowired
 	public MemberController(
-			MemberService memberService,
-			TokenProvider tokenProvider,
-			AuthMailService authMailService
+		MemberService memberService,
+		TokenProvider tokenProvider,
+		AuthMailService authMailService
 	) {
 		this.memberService = memberService;
 		this.tokenProvider = tokenProvider;
@@ -48,98 +48,97 @@ public class MemberController {
 	}
 
 	@ApiOperation(
-			value = "회원가입",
-			notes = "회원가입을 진행합니다."
+		value = "회원가입",
+		notes = "회원가입을 진행합니다."
 	)
 	@ApiResponses(
-			{
-					@ApiResponse(code = 200, message = "회원가입", response = MemberSaveResponseDto.class)
-			}
+		{
+			@ApiResponse(code = 200, message = "회원가입", response = MemberSaveResponseDto.class)
+		}
 	)
 	@PostMapping("/member/join")
 	public ResponseEntity<MemberSaveResponseDto> joinMember(
-			@RequestBody @Valid MemberSaveRequestDto memberSaveRequestDto
+		@RequestBody @Valid MemberSaveRequestDto memberSaveRequestDto
 	) {
 		return ResponseEntity.ok().body(memberService.joinMember(memberSaveRequestDto));
 	}
 
 	@ApiOperation(
-			value = "내정보 불러오기",
-			notes = "내정보를 불러옵니다."
+		value = "내정보 불러오기",
+		notes = "내정보를 불러옵니다."
 	)
 	@ApiResponses(
-			{
-					@ApiResponse(code = 200, message = "내정보", response = MemberInfoResponseDto.class)
-			}
+		{
+			@ApiResponse(code = 200, message = "내정보", response = MemberInfoResponseDto.class)
+		}
 	)
 	@GetMapping("/member/info")
 	public ResponseEntity<MemberInfoResponseDto> findMemberInfo(
-			@RequestHeader("Authorization") String token) {
+		@RequestHeader("Authorization") String token) {
 		return ResponseEntity.ok()
-				.body(memberService.findMemberInfo(tokenProvider.validateAndGetUserId(token.substring(7))));
+			.body(memberService.findMemberInfo(tokenProvider.validateAndGetUserId(token.substring(7))));
 	}
 
 	@ApiOperation(
-			value = "아이디로 내정보 불러오기",
-			notes = "아이디로 내정보를 불러옵니다."
+		value = "아이디로 내정보 불러오기",
+		notes = "아이디로 내정보를 불러옵니다."
 	)
 	@ApiResponses(
-			{
-					@ApiResponse(code = 200, message = "내정보", response = MemberInfoResponseDto.class)
-			}
+		{
+			@ApiResponse(code = 200, message = "내정보", response = MemberInfoResponseDto.class)
+		}
 	)
 	@GetMapping("/member/info/{memberId}")
 	public ResponseEntity<MemberInfoResponseDto> findMemberInfoByMemberId(
-			@PathVariable String memberId) {
+		@PathVariable String memberId) {
 		return ResponseEntity.ok()
-				.body(memberService.findMemberInfo(memberId));
+			.body(memberService.findMemberInfo(memberId));
 	}
 
 	@ApiOperation(
-			value = "비밀번호 변경",
-			notes = "비밀번호를 변경합니다."
+		value = "비밀번호 변경",
+		notes = "비밀번호를 변경합니다."
 	)
 	@ApiResponses(
-			{
-					@ApiResponse(code = 200, message = "변경된 사용자 아이디", response = String.class)
-			}
+		{
+			@ApiResponse(code = 200, message = "변경된 사용자 아이디", response = String.class)
+		}
 	)
 	@PutMapping("member/modify-password")
 	public String modifyMemberPassword(
-			@RequestHeader("Authorization") String token,
-			@RequestBody @Valid ModifyMemberPasswordRequestDto modifyMemberPasswordRequestDto
+		@RequestBody @Valid ModifyMemberPasswordRequestDto modifyMemberPasswordRequestDto
 	) {
-		return memberService.modifyMemberPassword(tokenProvider.validateAndGetUserId(token.substring(7)),
-				modifyMemberPasswordRequestDto);
+		return memberService.modifyMemberPassword(modifyMemberPasswordRequestDto.getMemberId(),
+			modifyMemberPasswordRequestDto);
 	}
 
 	@ApiOperation(
-			value = "카카오 소셜로그인 후 닉네임,유저구분,나이대,커리어 정보 설정",
-			notes = "카카오 소셜로그인 후 닉네임,유저구분,나이대,커리어 정보 설정합니다."
+		value = "카카오 소셜로그인 후 닉네임,유저구분,나이대,커리어 정보 설정",
+		notes = "카카오 소셜로그인 후 닉네임,유저구분,나이대,커리어 정보 설정합니다."
 	)
 	@ApiResponses(
-			{
-					@ApiResponse(code = 200, message = "변경된 사용자 아이디", response = String.class)
-			}
+		{
+			@ApiResponse(code = 200, message = "변경된 사용자 아이디", response = String.class)
+		}
 	)
 	@PutMapping("oauth/member/modify-member-info")
 	public String modifyMemberInfoAfterOauthKakaoSignIn(
-			@RequestHeader("Authorization") String token,
-			@RequestBody @Valid OAuthKakaoSignInRequestDto oAuthKakaoSignInRequestDto
+		@RequestHeader("Authorization") String token,
+		@RequestBody @Valid OAuthKakaoSignInRequestDto oAuthKakaoSignInRequestDto
 	) {
 		return memberService.modifyMemberInfoAfterOauthKakaoSignIn(
-				tokenProvider.validateAndGetUserId(token.substring(7)),
-				oAuthKakaoSignInRequestDto);
+			tokenProvider.validateAndGetUserId(token.substring(7)),
+			oAuthKakaoSignInRequestDto);
 	}
 
 	@ApiOperation(
-			value = "이메일로 아이디를 찾습니다.",
-			notes = "이메일로 아이디를 찾습니다."
+		value = "이메일로 아이디를 찾습니다.",
+		notes = "이메일로 아이디를 찾습니다."
 	)
 	@ApiResponses(
-			{
-					@ApiResponse(code = 200, message = "반환값 없음")
-			}
+		{
+			@ApiResponse(code = 200, message = "반환값 없음")
+		}
 	)
 	@GetMapping("/member/find/{email}")
 	public void findMemberIdByEmail(@PathVariable @Email(message = "올바른 이메일 형식이 아닙니다.") String email) {
--- lesson/src/main/java/project/lesson/dto/member/ModifyMemberPasswordRequestDto.java
+++ lesson/src/main/java/project/lesson/dto/member/ModifyMemberPasswordRequestDto.java
@@ -8,6 +8,9 @@ import lombok.Getter;
 @Getter
 public class ModifyMemberPasswordRequestDto {
 	@ApiModelProperty(example = "비밀번호", value = "비밀번호", required = true)
+	private String memberId;
+
+	@ApiModelProperty(example = "비밀번호", value = "비밀번호", required = true)
 	@Pattern(
 			regexp = "^(?=.*?[a-z])(?=.*?[0-9])(?=.*?[#?!@$%^&*-]).{8,}$",
 			message = "비밀번호는 8자 이상의 영문 소문자, 숫자, 특수문자가 포함되어야 합니다."
